afd34509fb00a2ada8fd01efcf80702986fcd380
Марина <marina.acmazikova@gmail.com>
2020-09-11 11:00:43 +0300

    new commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,11 +1,11 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -14,13 +14,14 @@ public class LoginTest {
 
     @BeforeEach
     public void initWebDriver() {
-        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "C:/users/Марина/chromedriver.exe");
         ChromeOptions option = new ChromeOptions();
         option.addArguments("--window-size=500,500");
-        WebDriver driver = new ChromeDriver(option);
+        driver = new ChromeDriver(option);
         driver.manage().window().setSize(new Dimension(200, 100));
         driver.navigate().to("https://tt-develop.quality-lab.ru");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @AfterEach
@@ -31,14 +32,13 @@ public class LoginTest {
     @Test
     public void incorrectUserNameAndPassword(){
         By elementLocator = By.xpath("//*[contains(text(), 'Invalid credentials')]");
-        driver.findElement(By.cssSelector("[name = '_username']")).sendKeys("TestUser");
-        driver.findElement(By.cssSelector("#password")).sendKeys("Password");
+        driver.findElement(By.cssSelector("[name=\"_username\"]")).sendKeys("TestUser");
+        driver.findElement(By.cssSelector("[id=\"password\"]")).sendKeys("Password");
         try {
             driver.findElement(elementLocator);
             fail();
         }catch (NoSuchElementException ignored){}
         driver.findElement(By.cssSelector("[value='Войти']")).click();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.findElement(elementLocator);
     }
 }
